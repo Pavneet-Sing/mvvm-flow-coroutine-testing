ffc96093b21ef19eef192cd435358c8703d8fa7a
Pavneet-Sing <dev.pavneet@gmail.com>
2020-03-25 14:48:51 +0530

    refactor: using factory provider and formatting

--- app/src/main/java/com/pavneet_singh/mvvm_flow_coroutine_testing/ui/main/ChocolateFragment.kt
+++ app/src/main/java/com/pavneet_singh/mvvm_flow_coroutine_testing/ui/main/ChocolateFragment.kt
@@ -1,15 +1,15 @@
 package com.pavneet_singh.mvvm_flow_coroutine_testing.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.pavneet_singh.mvvm_flow_coroutine_testing.R
-import com.pavneet_singh.mvvm_flow_coroutine_testing.ui.main.di.ChocoLateFactory
+import com.pavneet_singh.mvvm_flow_coroutine_testing.ui.main.di.FactoryProvider
 import com.pavneet_singh.mvvm_flow_coroutine_testing.ui.main.usecase.ChocolateUseCase
 
 /**
@@ -24,10 +24,12 @@ class ChocolateFragment : Fragment() {
         fun newInstance() = ChocolateFragment()
     }
 
-    private lateinit var viewModel : ChocolateViewModel
+    private lateinit var viewModel: ChocolateViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
@@ -38,14 +40,14 @@ class ChocolateFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val chocoLateFactory =  ChocoLateFactory(ChocolateUseCase())
-        viewModel = ViewModelProvider(this,chocoLateFactory).get(ChocolateViewModel::class.java)
+        val chocoLateFactory = FactoryProvider.getChocolateViewModelFactory(ChocolateUseCase())
+        viewModel = ViewModelProvider(this, chocoLateFactory).get(ChocolateViewModel::class.java)
         viewModel.onOptionsSelected().observe(viewLifecycleOwner, Observer {
-            if(it.loading){
+            if (it.loading) {
                 tvMessage.text = "Loading"
-            }else if(!it.loading && it.data.isNotEmpty()){
-                tvMessage.text = it.run{ "${it.data[0].name} Now run the test" }
-            }else{
+            } else if (!it.loading && it.data.isNotEmpty()) {
+                tvMessage.text = it.run { "${it.data[0].name} Now run the test" }
+            } else {
                 tvMessage.text = "Error"
             }
         })
